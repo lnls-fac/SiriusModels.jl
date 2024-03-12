5febcb926cba7524946decec21d03dfd13021f09
Vitor <vitor.souza@lnls.br>
2024-03-12 09:13:51 -0300

    MOD: deactivate precompilation (not working)

--- src/SiriusModels.jl
+++ src/SiriusModels.jl
@@ -1,18 +1,14 @@
 """Models module."""
 module SiriusModels
+
     include("SI/StorageRing.jl")
-    export StorageRing
 
-    using Track.Auxiliary
-    using PrecompileTools
+    # using PrecompileTools
 
-    @setup_workload begin
-        @compile_workload begin
-            m = StorageRing.create_accelerator()
-            m.radiation_state = Auxiliary.full
-            m.cavity_state = Auxiliary.on
-            m.vchamber_state = Auxiliary.on
-        end
-    end
+    # @setup_workload begin
+    #     @compile_workload begin
+    #         m = StorageRing.create_accelerator()
+    #     end
+    # end
 
 end # module SiriusModels
